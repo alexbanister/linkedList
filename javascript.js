d66b1e9fba7baf6eb1d04b23021e258c76a9cd18
Alex Banister <alexbanister@gmail.com>
2017-07-11 12:13:41 -0600

    add cleanHTTP function for setting url

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
+// If the user omits the title or the URL, the application should not create the link and should instead display an error.
+//   only disables button, no error displayed yet
+//
+// The application should be responsive and work equally well on desktop and mobile.
+//   fix mobile landscape display
+//
+// The application should be able to keep count of the total number of links currently on the page.
+//   add querySelectorAll('article').length and display it
+//
+// The application should be able to keep count of the total number of read and unread links currently on the page.
+//   add querySelectorAll('read').length and display it
+//   add above article.length - read.length and display it
+//
+// Add a “Clear Read Bookmarks” button which clears all the read bookmarks when clicked.
+
 //Global Variables
 var enterButton = document.getElementById("enter-button");
 var markAsReadButton = document.getElementById("mark-as-read-button");
@@ -8,12 +23,13 @@ var websiteUrl = document.getElementById("website-url")
 
 //Objects
 var linkBox = {
-  topHTML: '</h2><h3><a href="',
+  topHTML: '</h2><h3><a href="http://',
   bottomHtml: '</a></h3><div class="read-delete-links"><p class="read-link"><a href="#" class="read-target">Read</a></p><p><a href="#" class="delete-target">Delete</a></p></div>',
   buildIt: function(title, url){
+    url = cleanHTTP(url);
     var boxHTML = document.createElement("article");
     boxHTML.className = "website-box";
-    boxHTML.innerHTML = '<h2>' + title + this.topHTML +  url + '">' +  url + this.bottomHtml;
+    boxHTML.innerHTML = '<h2>' + title + this.topHTML +  url + '" target="_new">' +  url + this.bottomHtml;
 
     mainContentBox.insertAdjacentHTML("afterbegin", boxHTML.outerHTML);
   }
@@ -25,29 +41,34 @@ enterButton.addEventListener("click", function(e){
   linkBox.buildIt(websiteTitle.value, websiteUrl.value);
 });
 
-websiteUrl.addEventListener ("blur", function(){
-  var isValid = isValidURL(websiteUrl.value);
-  if (isValid) {
-    enterButton.disabled = false;
-  } else {
-    enterButton.disabled = true;
-  }
-});
+websiteUrl.addEventListener ("input", validateForm);
 
-websiteTitle.addEventListener ("input", function(){
-  if (websiteTitle.value === "") {
-    enterButton.disabled = true;
-  } else {
-    enterButton.disabled = false;
-  }
-});
+websiteTitle.addEventListener ("input", validateForm);
 
 mainContentBox.addEventListener("click", websiteBoxLinks, false);
 
 //Functions
 function isValidURL(url) {
-  var regEx=new RegExp("^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$");
+  var regEx=new RegExp("^((https?):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$");
   return regEx.test(url);
+}
+
+function cleanHTTP(url) {
+  if (!url.match(/^[a-zA-Z]+:\/\//)) {
+    return url;
+  } else {
+    var urlSplit = url.split("//");
+    var urlArray = urlSplit.slice(1);
+    return urlArray.toString();
+  }
+}
+
+function validateForm() {
+  if (isValidURL(websiteUrl.value) && websiteTitle.value !== "") {
+    enterButton.disabled = false;
+  } else {
+    enterButton.disabled = true;
+  }
 }
 
 function websiteBoxLinks(e) {
